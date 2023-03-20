c8b1f5dc8cb616e333118a0c379c8c1d0790b2d7
sinadaryaei <sinatangssiran@gmail.com>
2023-03-20 19:31:44 +0000

    javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,34 @@
+
+// eslint-disable-next-line no-unused-vars, no-undef
+const fullName = 'Sina Daryaei';
+
+console.log('value of fullName:', fullName);
+
+// eslint-disable-next-line no-unused-vars
+const isCool = true;
+
+console.log('value of isCool:', isCool);
+
+// eslint-disable-next-line no-unused-vars
+const totalPets = 2;
+
+console.log('value of totalPets:', totalPets);
+
+console.log('tyeof fullName:', typeof fullName);
+
+console.log('typeof of isCool:', typeof isCool);
+
+console.log('typeof of totalPets:', typeof totalPets);
+
+// eslint-disable-next-line no-unused-vars
+const unknown = null;
+
+console.log('value of unknown:', unknown);
+
+console.log('typeof of unknown:', typeof unknown);
+
+let never;
+
+console.log('value of never:', never);
+
+console.log('type of never:', typeof never);
