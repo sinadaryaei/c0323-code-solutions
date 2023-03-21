1105967d9193e184fc2fcf494395c8292e1bff5b
sinadaryaei <sinatangssiran@gmail.com>
2023-03-21 17:51:40 +0000

    javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,31 @@
+var orderHistory = [
+  {
+    Order: 'August 4, 2020',
+    Total: '34.00',
+    Shiping: 'Js Masher',
+    OrderNumber: '114-3941689-8772232',
+    Item: 'Javascript for impatient programmers'
+  },
+  {
+    Order: 'July 19, 2020',
+    Total: '44.53',
+    Shiping: 'Js Masher',
+    OrderNumber: '114-9984268-1280257',
+    Item: 'The Timeless Way of Building'
+  },
+  {
+    Order: 'August 4, 2020',
+    Total: '17.22',
+    Shiping: 'Js Masher',
+    OrderNumber: '114-2875557-9059409',
+    Item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter'
+  },
+  {
+    Order: 'July 3, 2020',
+    Total: '137.93',
+    Shiping: 'Js Masher',
+    OrderNumber: '113-2883177-2648248',
+    Item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)' + 'The Art of Sql'
+  }
+];
+console.log('Order history:', orderHistory);
