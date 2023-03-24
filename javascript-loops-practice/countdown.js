ffba7882314b4959611a6769d49273163c954978
sinadaryaei <sinatangssiran@gmail.com>
2023-03-24 14:09:47 +0000

    javascript-loops-practice

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -1,1 +1,10 @@
 /* exported countdown */
+function countdown(number) {
+  const result = [];
+  let countdown = number;
+  while (countdown >= 0) {
+    result.push(countdown);
+    countdown--;
+  }
+  return result;
+}
