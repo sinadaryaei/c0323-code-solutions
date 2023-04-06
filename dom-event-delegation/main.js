3b1f4b9f95acb1a20481500869963badda790073
sinadaryaei <sinatangssiran@gmail.com>
2023-04-06 19:26:11 +0000

    dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,5 @@
+// const $taskList = document.querySelector('.task-list');
+// $taskList.addEventListener('click', function (event) {
+//   console.log(event.target);
+//   console.log(event.target.tagName);
+//   if (event.target && event.target.tagName === 'BUTTON') {
