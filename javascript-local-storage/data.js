289563d8bc4eb505c42357f5f3e0d00197bbf564
sinadaryaei <sinatangssiran@gmail.com>
2023-04-10 01:30:32 +0000

    javascript-local-storage

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,6 +1,11 @@
-// /* exported todos */
+/* exported todos */
 
-// let todos = [];
-// window.addEventListener('beforeunload', (event) => {
-//   const todosJSON = JSON.stringify('todos');
-// });
+let todos = [];
+window.addEventListener('beforeunload', function () {
+  const todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+});
+const previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
