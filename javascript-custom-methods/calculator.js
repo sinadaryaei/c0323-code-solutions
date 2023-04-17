41b7f8b846fd1f2cfefd79e1a4079fe090088352
sinadaryaei <sinatangssiran@gmail.com>
2023-04-17 20:54:12 +0000

    javascript-custom-methods

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,19 @@
+/* exported calculator */
+
+const calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  sqaure: function (x) {
+    return Math.pow(x, 2);
+  }
+};
